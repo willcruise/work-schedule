52f687b2b91d26512c46f9ccc10b6f0ff4f1bdb0
willcruise <100233346+willcruise@users.noreply.github.com>
2024-04-01 20:17:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import calendar
 import datetime
 
-monthrange = calendar.monthrange(datetime.date.today.year(), datetime.date.today.month())
+print("Enter year and month: 0000-00")
+yearmonth = input()
 
-w = input()
+datelist = yearmonth.split("-")
+datelist[1] = datelist[1].replace("0","")
 
-worker = string.replace(split())
+datetime_date = datetime.date(int(datelist[0]), int(datelist[1]), 1)
+firstdayofweek = datetime_date.weekday()
 
+print(firstdayofweek)
