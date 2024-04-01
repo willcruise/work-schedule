951c5b1893aae48881189599b1d17c39c3536bb4
willcruise <100233346+willcruise@users.noreply.github.com>
2024-04-01 20:38:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ datelist[1] = datelist[1].replace("0","")
 
 datetime_date = datetime.date(int(datelist[0]), int(datelist[1]), 1)
 firstdayofweek = datetime_date.weekday()
+monthrange = calendar.monthrange(int(datelist[0]), int(datelist[1]))[1]
 
-print(firstdayofweek)
+calen = []
+
+for i in range(monthrange):
+    calen.append([i+1, firstdayofweek])
+    if firstdayofweek < 6:
+        firstdayofweek += 1
+    else : firstdayofweek = 0
+   
+print(calen)
