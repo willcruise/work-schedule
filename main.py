1b93941f3930e35a131fb930a2d11d5182aa4806
willcruise <100233346+willcruise@users.noreply.github.com>
2024-04-03 20:42:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import calendar
 import datetime
 
-print("Enter year and month: 0000-00")
+print("Enter year and month: year-month ex)2024-3")
 yearmonth = input()
 """get year and month of the making schedule"""
 
 
 datelist = yearmonth.replace(" ", "").split("-")
-datelist[1] = datelist[1].replace("0","")
+datelist[1] = datelist[1]
 
 datetime_date = datetime.date(int(datelist[0]), int(datelist[1]), 1)
 firstdayofweek = datetime_date.weekday()
 monthrange = calendar.monthrange(int(datelist[0]), int(datelist[1]))[1]
 """get monthrange and day of week of the first day of month""" 
 
-print("Enter the offdays: 00, 00, ...")
+print("Enter the offdays: day1, day2, ... ex)4,5,6,15,29")
 offdays = input()
-offdays = offdays.replace(" ","").replace("0","").split(",")
+offdays = offdays.replace(" ","").split(",")
 
 for i in range(len(offdays)): 
     offdays[i] = int(offdays[i])
-    
-"""offdays_date = []
-for i in offdays:
-    date = datetime.date(int(datelist[0]), int(datelist[1]), int(i))
-    print(date)
-    offdays_date.append(date)"""
-"""get offdays"""
-
-
 
 calen = []
 
@@ -38,35 +29,31 @@ for i in range(monthrange):
         firstdayofweek += 1
     else : firstdayofweek = 0
 
-first = True
-for i in calen:
-    m = 0
-    match = False
-    for c in offdays:
-        if i[0] == c:
-            if first == True: 
-                calen[m][1] = 5
-            else : calen[m][1] = 6
-            match = True
-    if match == True : first = False
-    else : first = True
-    m += 1    
-print(calen)        
+for i in offdays:
+    for c in range(len(calen)):
+        if i == calen[c][0]:
+            calen[c][1] = 5
+
+yesteroff = False
+for i in range(len(calen)):
+    if calen[i][1] == 5 or calen[i][1] == 6:
+        if yesteroff == True: calen[i][1] = 6
+        yesteroff = True
+    else: yesteroff = False
 """get the calender for the month of the schedule"""
-   
-   
-schedule = {}
+  
 
-for i in range(monthrange):
-    if  calen[i][2] <= 3:
-        schedule[calen[i]] =["평야"]
-    elif calen[i][2] == 4:
-        schedule[calen[i]] = ["금야"]
-    elif calen[i][2] == 5:
-        schedule[calen[i]] = ["토주","토야"]
-    elif calen[i][2] == 6:
-        schedule[calen[i]] = ["일주","일야"]
+for i in range(len(calen)):
+    if  calen[i][1] <= 3:
+        calen[i].append(["평야"])
+    elif calen[i][1] == 4:
+        calen[i].append(["금야"])
+    elif calen[i][1] == 5:
+        calen[i].append(["토주","토야"])
+    elif calen[i][1] == 6:
+        calen[i].append(["일주","일야"])
 
+print(calen)
 
     
     
