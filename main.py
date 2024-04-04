6b42ed6ce5eb8e7cf8cb4efe9cb3927ce4e0104e
willcruise <100233346+willcruise@users.noreply.github.com>
2024-04-04 12:04:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ firstdayofweek = datetime_date.weekday()
 monthrange = calendar.monthrange(int(datelist[0]), int(datelist[1]))[1]
 """get monthrange and day of week of the first day of month""" 
 
-print("Enter the offdays: day1, day2, ... ex)4,5,6,15,29")
-offdays = input()
-offdays = offdays.replace(" ","").split(",")
+print("Enter the holidays: day1, day2, ... ex)4,5,6,15,29")
+holidays = input()
+holidays = holidays.replace(" ","").split(",")
 
 for i in range(len(offdays)): 
-    offdays[i] = int(offdays[i])
-"""get offdays for the month"""
+    holidays[i] = int(offdays[i])
+"""get holidays for the month"""
 
 
 calen = []
@@ -31,7 +31,7 @@ for i in range(monthrange):
         firstdayofweek += 1
     else : firstdayofweek = 0
 
-for i in offdays:
+for i in holidays:
     for c in range(len(calen)):
         if i == calen[c][0]:
             calen[c][1] = 5
@@ -42,6 +42,7 @@ for i in range(len(calen)):
         if yesteroff == True: calen[i][1] = 6
         yesteroff = True
     else: yesteroff = False
+        
 """get the calender for the month of the schedule"""
   
 
@@ -57,14 +58,25 @@ for i in range(len(calen)):
 
 print(calen)
 
+print("Enter the vacations and dayoffs of workers: name1: day1 ~ day2, day3, name2: ... ex)홍길동: 10 ~ 19, 25, 홍길둥: 14")
+dayoffs = input()
+dayoffs = dayoffs.replace(" ", "")
+dayoffs = dayoffs.split(":")
+
     
-    
-"""get vacations, outdoordays for workers"""
+"""get vacations, dayoffs for workers"""
 
 """allot duties for workers"""
 
 """regression process"""
-    
+
+"""1. make calendars regarding to weights.
+     allot specific weights to each duty types, 
+     and group duties for each workers concerning the vacations, and dayoffs
+    2. test the calenders built from the previous step by work counts, and distance between days of work
+       alloted to each workers.
+    3. evaluate the final schedule
+    """
     
 """final schedule"""
     
